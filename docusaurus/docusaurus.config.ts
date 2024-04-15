0151a3fd533975db35abd4ba1fb180039cdae2f5
Cid Huang <cidhuang@gmail.com>
2024-04-15 13:16:27 +0800

    docs: change doc of code settings

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -95,7 +95,11 @@ const config: Config = {
 
       // Plugin / TypeDoc options
       {
-        entryPoints: ['../src/app/*.tsx'],
+        entryPoints: [
+          '../src/app/**/*.ts',
+          '../src/components/**/*.ts',
+          '../src/lib/**/*.ts'
+        ],
         tsconfig: '../tsconfig.json',
         out: 'code',
         sidebar: {
