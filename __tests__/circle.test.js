186452b21d212af352f1390e2bcfebc9aebac22a
Cid Huang <cidhuang@gmail.com>
2024-04-19 18:12:46 +0800

    fix: test error due to getCircle modification

--- __tests__/circle.test.js
+++ __tests__/circle.test.js
@@ -9,7 +9,7 @@ describe("Check getCircle", () => {
   test("Check the result of getCircle", () => {
     expect(
       getCircle({ x: 0, y: 0 }, { x: 100, y: 100 }, { x: 200, y: 0 })
-    ).toStrictEqual({ cx: 100, cy: 0, radius: 100 });
+    ).toStrictEqual({ x: 100, y: 0, radius: 100 });
   });
 });
 
